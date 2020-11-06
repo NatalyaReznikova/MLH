da4dbc8ebeb4ff671921b3d88ea68dd76c8c93f8
NatalyaReznikova <natalyavasylyeva@yahoo.com>
2020-11-06 10:54:16 -0800

    add inputValues4

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,12 +17,15 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/smoke/*.js'
+        './test/smoke/inputs.js',
+        './test/smoke/labels.js',
+        './test/smoke/mainFunctionality.js'
 
     ],
     // Patterns to exclude.
     exclude: [
-        // 'path/to/excluded/files'
+        './test/smoke/inputs.js',
+        './test/smoke/labels.js'
     ],
     //
     // ============
